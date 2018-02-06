ed63d79c10228d8728656a78635af8fade630a94
Jonathan Zia <jonathan.s.zia@gmail.com>
2018-02-06 10:56:33 -0500

    Updated FeatureExtraction for one-hot classification

--- FeatureExtraction.m
+++ FeatureExtraction.m
@@ -1,15 +1,16 @@
 % -------------------------------------------------------------------------
 % Dataset Feature Extraction
 % Created by: Jonathan Zia
-% Last Edited: Saturday, Feb 3 2018
+% Last Edited: Monday, Feb 5 2018
 % Georgia Institute of Technology
 % -------------------------------------------------------------------------
 
 % This program imports and processes .csv file data files for use in LSTM
 % network prediction and classification tasks.
+clear;clc
 
 % Specify output filenames for writing processed data to .csv files:
-output_filenames = ["S01R01_t.csv","S01R02_t.csv"];
+output_filenames = ["S01R01_t.csv", "S01R02_t.csv"];
 
 
 %% ------------------------------------------------------------------------
@@ -44,6 +45,53 @@ for file = 1:num_files(2)   % For each file...
     end
 end
 
+% Append datafiles with two extra label columns for one-hot three-category
+% classification: non-FoG, pre-FoG, and FoG.
+for file = 1:num_files(2)   % For each file...
+    num_elem = size(data{file});   % Obtain number of datapoints
+    % Append two zero-columns
+    data{file} = [data{file}, zeros(num_elem(1),1), zeros(num_elem(1),1)];
+end
+
+% Update labels into one-hot three-category classification:
+% non-FoG:  [1 0 0]
+% pre-FoG:  [0 1 0] "non-FoG" datapoints occuring within 'n' steps of FoG
+n = 500; % Size of pre-FoG window
+% FoG:      [0 0 1]
+for file = 1:num_files(2)   % For each file...
+    num_elem = size(data{file});   % Obtain number of datapoints
+    
+    % Update non-FoG and FoG labels
+    for i = 1:num_elem(1)   % For each datapoint in file...
+        if isequal(data{file}(i,11:13),[0 0 0])    % If it is a non-FoG event...
+            data{file}(i,11:13) = [1 0 0];  % Update labels
+        elseif isequal(data{file}(i,11:13),[1 0 0])  % If it is an FoG event...
+            data{file}(i,11:13) = [0 0 1];  % Update labels
+        end
+    end
+    
+    % Update pre-FoG labels
+    for i = 1:num_elem(1)-1   % For each datapoint in file...
+        % If FoG occurs
+        if isequal(data{file}(i,11:13),[1 0 0]) && isequal(data{file}(i+1,11:13),[0 0 1])
+            % Update pre-FoG labels
+            for j = i:-1:i-n % For the n timesteps before the FoG event
+                data{file}(j,11:13) = [0 1 0];  % Update labels
+            end
+        end
+    end
+end
+
+% Update labels into one-hot three-category classification:
+% non-FoG:  [1 0 0]
+% pre-FoG:  [0 1 0] "non-FoG" datapoints occuring within 'n' steps of FoG
+% FoG:      [0 0 1]
+for file = 1:num_files(2)   % For each file...
+    num_elem = size(data{file});   % Obtain number of datapoints
+    for i = 1:num_elem(1)   % For each datapoint in file...
+        
+    end
+end
 
 %% ------------------------------------------------------------------------
 % Create Output CSV Files
