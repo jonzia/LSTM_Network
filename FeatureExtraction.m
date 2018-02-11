8155d4ec30eeba3344c016ad33cbe17963c96879
Jonathan Zia <jonathan.s.zia@gmail.com>
2018-02-10 23:09:32 -0500

    Update to v1.3.1

--- FeatureExtraction.m
+++ FeatureExtraction.m
@@ -27,6 +27,10 @@ freq_bins = 9;
 % Specify output filenames for writing processed data to .csv files:
 output_filenames = [""];
 
+% Specify number of features and output classes
+feature_num = 9;
+class_num = 3;
+
 
 %% ------------------------------------------------------------------------
 % Import CSV Data
@@ -38,116 +42,59 @@ num_files = size(filename_list);
 % Import data into array of size [num_files x data_points x data_columns]
 data = cell(num_files(2), 1);
 for i = 1:num_files(2)
-data{i} = importdata(filename_list(i));
+    data{i} = importdata(filename_list(i));
 end
 
 
 %% ------------------------------------------------------------------------
 % Data Processing
 % -------------------------------------------------------------------------
-% Remove datapoints with "0" labels and decrement nonzero labels
-for file = 1:num_files(2)   % For each file...
-num_elem = size(data{file});   % Obtain number of datapoints
-num_removed = 0; % Number of datapoints removed during processing
-for i = 1:num_elem(1)   % For each datapoint in file...
-loop = i - num_removed; % Adjust loop counter for removed points
-if data{file}(loop,end) == 0   % If the label is "0"...
-data{file}(loop,:) = [];   % Remove the row
-num_removed = num_removed + 1; % Increment number of datapoints
-else    % Else...
-data{file}(loop,end) = data{file}(loop,end) - 1;  % Decrement label
-end
-end
-end
+% Insert data preprocessing code here
 
-% Append datafiles with two extra label columns for one-hot three-category
-% classification: non-FoG, pre-FoG, and FoG.
-for file = 1:num_files(2)   % For each file...
-num_elem = size(data{file});   % Obtain number of datapoints
-% Append two zero-columns
-data{file} = [data{file}, zeros(num_elem(1),1), zeros(num_elem(1),1)];
-end
-
-% Update labels into one-hot three-category classification:
-% non-FoG:  [1 0 0]
-% pre-FoG:  [0 1 0] "non-FoG" datapoints occuring within 'n' steps of FoG
-n = 500; % Size of pre-FoG window
-% FoG:      [0 0 1]
-for file = 1:num_files(2)   % For each file...
-num_elem = size(data{file});   % Obtain number of datapoints
-
-% Update non-FoG and FoG labels
-for i = 1:num_elem(1)   % For each datapoint in file...
-if isequal(data{file}(i,11:13),[0 0 0])    % If it is a non-FoG event...
-data{file}(i,11:13) = [1 0 0];  % Update labels
-elseif isequal(data{file}(i,11:13),[1 0 0])  % If it is an FoG event...
-data{file}(i,11:13) = [0 0 1];  % Update labels
-end
-end
-
-% Update pre-FoG labels
-for i = 1:num_elem(1)-1   % For each datapoint in file...
-% If FoG occurs
-if isequal(data{file}(i,11:13),[1 0 0]) && isequal(data{file}(i+1,11:13),[0 0 1])
-% Update pre-FoG labels
-for j = i:-1:i-n % For the n timesteps before the FoG event
-data{file}(j,11:13) = [0 1 0];  % Update labels
-end
-end
-end
-end
-figure(1)
-hold on
-plot(data{1}(:,2))
-hold off
 
 %% ------------------------------------------------------------------------
 % Low-Pass Time-Series Filtering
 % -------------------------------------------------------------------------
 if LPF
-% FIR low-pass filter designed by Filter Designer
-% Specifications: Fs = 67Hz (estimated); Fpass = 5Hz; Fstop = 10Hz
-coeff = [0.000513597053186222,0.00143953792902160,0.00268728781999995,0.00348885290937029,0.00255573006377796,-0.00151369188523023,-0.00942351770093767,-0.0202580500631260,-0.0309209563782769,-0.0364032357005344,-0.0310787936017955,-0.0107642471154336,0.0251755590489557,0.0725937065908270,0.122913328882096,0.165199949276606,0.189363597050808,0.189363597050808,0.165199949276606,0.122913328882096,0.0725937065908270,0.0251755590489557,-0.0107642471154336,-0.0310787936017955,-0.0364032357005344,-0.0309209563782769,-0.0202580500631260,-0.00942351770093767,-0.00151369188523023,0.00255573006377796,0.00348885290937029,0.00268728781999995,0.00143953792902160,0.000513597053186222];
-% Specifications: Fs = 67Hz (estimated); Fpass = 1Hz; Fstop = 5Hz
-coeff2 = [4.79327748470482e-06,0.000263147414205478,0.000563196897790071,0.00112175527826935,0.00198999107314703,0.00325452633957827,0.00499929894323254,0.00729772496385976,0.0102035785723195,0.0137415157978823,0.0178995587752836,0.0226229466606835,0.0278114170707222,0.0333199221973773,0.0389640757832623,0.0445284393677857,0.0497795305685804,0.0544804071594470,0.0584072222157913,0.0613654721249311,0.0632042118363297,0.0638280730984335,0.0632042118363297,0.0613654721249311,0.0584072222157913,0.0544804071594470,0.0497795305685804,0.0445284393677857,0.0389640757832623,0.0333199221973773,0.0278114170707222,0.0226229466606835,0.0178995587752836,0.0137415157978823,0.0102035785723195,0.00729772496385976,0.00499929894323254,0.00325452633957827,0.00198999107314703,0.00112175527826935,0.000563196897790071,0.000263147414205478,4.79327748470482e-06];
-% Filtering data columns
-for file = 1:num_files(2) % For each file...
-% Filter columns in each file
-data{file}(:,2:10) = filter(coeff,1,data{file}(:,2:10),[],1);
+    % FIR low-pass filter designed by Filter Designer
+    % Specifications: Fs = 67Hz (estimated); Fpass = 5Hz; Fstop = 10Hz
+    coeff = [0.000513597053186222,0.00143953792902160,0.00268728781999995,0.00348885290937029,0.00255573006377796,-0.00151369188523023,-0.00942351770093767,-0.0202580500631260,-0.0309209563782769,-0.0364032357005344,-0.0310787936017955,-0.0107642471154336,0.0251755590489557,0.0725937065908270,0.122913328882096,0.165199949276606,0.189363597050808,0.189363597050808,0.165199949276606,0.122913328882096,0.0725937065908270,0.0251755590489557,-0.0107642471154336,-0.0310787936017955,-0.0364032357005344,-0.0309209563782769,-0.0202580500631260,-0.00942351770093767,-0.00151369188523023,0.00255573006377796,0.00348885290937029,0.00268728781999995,0.00143953792902160,0.000513597053186222];
+    % Specifications: Fs = 67Hz (estimated); Fpass = 1Hz; Fstop = 5Hz
+    coeff2 = [4.79327748470482e-06,0.000263147414205478,0.000563196897790071,0.00112175527826935,0.00198999107314703,0.00325452633957827,0.00499929894323254,0.00729772496385976,0.0102035785723195,0.0137415157978823,0.0178995587752836,0.0226229466606835,0.0278114170707222,0.0333199221973773,0.0389640757832623,0.0445284393677857,0.0497795305685804,0.0544804071594470,0.0584072222157913,0.0613654721249311,0.0632042118363297,0.0638280730984335,0.0632042118363297,0.0613654721249311,0.0584072222157913,0.0544804071594470,0.0497795305685804,0.0445284393677857,0.0389640757832623,0.0333199221973773,0.0278114170707222,0.0226229466606835,0.0178995587752836,0.0137415157978823,0.0102035785723195,0.00729772496385976,0.00499929894323254,0.00325452633957827,0.00198999107314703,0.00112175527826935,0.000563196897790071,0.000263147414205478,4.79327748470482e-06];
+    % Filtering data columns
+    for file = 1:num_files(2) % For each file...
+        % Filter columns in each file
+        data{file}(:,2:feature_num+1) = filter(coeff,1,data{file}(:,2:feature_num+1),[],1);
+    end
 end
-end
-figure(2)
-hold on
-plot(data{1}(:,2))
-hold off
 
 %% ------------------------------------------------------------------------
 % RMS Calculation
 % -------------------------------------------------------------------------
 if RMS
-% Create placeholder for data output
-data_rms = cell(num_files(2),1);
+    % Create placeholder for data output
+    data_rms = cell(num_files(2),1);
 
-% For each data file...
-for file = 1:num_files(2)
+    % For each data file...
+    for file = 1:num_files(2)
 
-% Obtain number of samples in file
-num_elem = size(data{file});
-% Initialize placeholders
-root_mean_square = zeros(num_elem(1)-window_size,num_elem(2));
+        % Obtain number of samples in file
+        num_elem = size(data{file});
+        % Initialize placeholders
+        root_mean_square = zeros(num_elem(1)-window_size,num_elem(2));
 
-% For each sample, obtain FFT columnwise
-for sample = 1:num_elem(1)-window_size
-% Obtain the RMS over bin_size samples
-root_mean_square(sample,2:10) = rms(data{file}(sample:sample+window_size,2:10),1);
-% Add timestamps to root_mean_square
-root_mean_square(sample,1) = data{file}(sample+window_size,1);
-% Add labels to root_mean_square
-root_mean_square(sample,11:13) = data{file}(sample+window_size,11:13);
-end
-% Update data file for exporting
-data_rms{file} = root_mean_square;
-end
+        % For each sample, obtain FFT columnwise
+        for sample = 1:num_elem(1)-window_size
+            % Obtain the RMS over bin_size samples
+            root_mean_square(sample,2:feature_num+1) = rms(data{file}(sample:sample+window_size,2:feature_num+1),1);
+            % Add timestamps to root_mean_square
+            root_mean_square(sample,1) = data{file}(sample+window_size,1);
+            % Add labels to root_mean_square
+            root_mean_square(sample,end-class_num+1:end) = data{file}(sample+window_size,end-class_num+1:end);
+        end
+        % Update data file for exporting
+        data_rms{file} = root_mean_square;
+    end
 end
 
 
@@ -155,62 +102,62 @@ end
 % Fourier Analysis
 % -------------------------------------------------------------------------
 if FA
-% Create placeholder for data output
-data_fa = cell(num_files(2),1);
+    % Create placeholder for data output
+    data_fa = cell(num_files(2),1);
 
-% For each data file...
-for file = 1:num_files(2)
+    % For each data file...
+    for file = 1:num_files(2)
 
-% Obtain number of samples in file
-num_elem = size(data{file});
-% Initialize placeholders
-fourier = cell(num_elem(1)-window_size,1);
-fourier_av = zeros(num_elem(1)-window_size,freq_bins+4);
+        % Obtain number of samples in file
+        num_elem = size(data{file});
+        % Initialize placeholders
+        fourier = cell(num_elem(1)-window_size,1);
+        fourier_av = zeros(num_elem(1)-window_size,freq_bins+class_num+1);
 
-% For each sample, obtain FFT columnwise
-for sample = 1:num_elem(1)-window_size
-% Obtain the FFT over bin_size samples
-raw_freq = fft(data{file}(sample:sample+window_size,2:10),[],1);
-raw_freq = abs(raw_freq/window_size); % Convert raw_freq to scalar
-one_freq = raw_freq(1:(window_size/2)+1,:); % Obtain one-sided FFT
-one_freq = 2*one_freq(2:end-1,:); % Scale one-sied FFT by 2
-fourier{sample} = one_freq;
-% Adding timestamp to fourier_av
-fourier_av(sample,1) = data{file}(sample+window_size,1);
-% Adding label to fourier_av
-fourier_av(sample,end-2:end) = data{file}(sample+window_size,11:13);
-% Adding frequency bin average to fourier_av
-% Calculate points per bin
-points_bin = floor((window_size/2-1)/freq_bins);
-% For each bin...
-for i = 1:freq_bins
-% For all bins before the final one...
-if i < freq_bins
-% Calculate mean of all points in bin
-fourier_av(sample,i+1) = mean(mean(fourier{sample}((i-1)*points_bin+1:(i-1)*points_bin+points_bin,:)));
-else % Else, for the last bin...
-% Average over the remaining number of points
-fourier_av(sample,i+1) = mean(mean(fourier{sample}((i-1)*points_bin+1:end,:)));
-end
-end
-end
-% Update data file for exporting
-data_fa{file} = fourier_av;
-end
+        % For each sample, obtain FFT columnwise
+        for sample = 1:num_elem(1)-window_size
+            % Obtain the FFT over bin_size samples
+            raw_freq = fft(data{file}(sample:sample+window_size,2:feature_num+1),[],1);
+            raw_freq = abs(raw_freq/window_size); % Convert raw_freq to scalar
+            one_freq = raw_freq(1:(window_size/2)+1,:); % Obtain one-sided FFT
+            one_freq = 2*one_freq(2:end-1,:); % Scale one-sied FFT by 2
+            fourier{sample} = one_freq;
+            % Adding timestamp to fourier_av
+            fourier_av(sample,1) = data{file}(sample+window_size,1);
+            % Adding label to fourier_av
+            fourier_av(sample,end-2:end) = data{file}(sample+window_size,end-class_num+1:end);
+            % Adding frequency bin average to fourier_av
+            % Calculate points per bin
+            points_bin = floor((window_size/2-1)/freq_bins);
+            % For each bin...
+            for i = 1:freq_bins
+                % For all bins before the final one...
+                if i < freq_bins
+                    % Calculate mean of all points in bin
+                    fourier_av(sample,i+1) = mean(mean(fourier{sample}((i-1)*points_bin+1:(i-1)*points_bin+points_bin,:)));
+                else % Else, for the last bin...
+                    % Average over the remaining number of points
+                    fourier_av(sample,i+1) = mean(mean(fourier{sample}((i-1)*points_bin+1:end,:)));
+                end
+            end
+        end
+        % Update data file for exporting
+        data_fa{file} = fourier_av;
+    end
 end
 
 
 %% ------------------------------------------------------------------------
 % Create Output CSV Files
 % -------------------------------------------------------------------------
-Write updated arrays to .csv file
+% Write updated arrays to .csv file
 for file = 1:num_files(2)   % For each file...
-if FA % If Fourier analysis was performed
-% Write data to name.csv
-csvwrite(output_filenames(file),data_fa{file});
-elseif RMS % If RMS analysis was performed
-csvwrite(output_filenames(file),data_rms{file});
-else % If LPF or no analysis was performed
-csvwrite(output_filenames(file),data{file});
-end
+    if FA % If Fourier analysis was performed
+        % Write data to name.csv
+        csvwrite(output_filenames(file),data_fa{file});
+    elseif RMS % If RMS analysis was performed
+        csvwrite(output_filenames(file),data_rms{file});
+    else % If LPF or no analysis was performed
+        csvwrite(output_filenames(file),data{file});
+    end
 end
